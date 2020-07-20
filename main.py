3eebdf15abbc23da6b2979f80bcee0e52843aa1e
Pratik Kotian <100-admin@EDL-100.exponentia.ai>
2020-07-20 21:43:47 +0530

    exception handling

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ import os
 
 
 def read_file(file_name, start=None, end=None):
-    
-    file = open(file_name,'rb')
-    if start != None and end != None:
+    try:
+        file = open(file_name,'rb')
+        if start != None and end != None:
 
-    	return [str(i) for i in file.readlines()[int(start):int(end)]]
-    else:
-        return [str(i) for i in file.readlines()]
+            return [str(i) for i in file.readlines()[int(start):int(end)]]
+        else:
+            return [str(i) for i in file.readlines()]
+        
+    except:
+        return ["<b>Error is file reading.Please sepcify another file</b>"]
     
     
     
